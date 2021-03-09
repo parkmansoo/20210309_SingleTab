c4eecd5c968659b7640e6524044d7843fc4be36a
parkmansoo <qkrakstn02@gmail.com>
2021-03-09 16:22:39 +0900

    멀티탭_구현중

---
+++ script/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+});
+
+$(function(){
+    $("button:nth-child(2)").click(function(){
+        $("button").css({
+            "border":"none",
+            "top":"0px"
+        });
+
+        $(this).css({
+            "border":"1px solid #aaa",
+            "border-bottom":"none",
+            "top":"1px"
+        });
+
+    });
+});
